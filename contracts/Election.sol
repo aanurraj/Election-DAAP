1c8307f98fb668d5cc5681fa931a384eb5a8b968
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2019-09-10 01:17:38 +0530

    Smart Contract Ready, Awaited test using Mocha and Chai

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract Election{
-    string public candidate;
+
+    // Defining Structure of Candidate in Election
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCode;
+    }
+
+    // Storing and Fetching Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Saving Candidate Count to fetech data from map using loop (Cant iterate or determine size of mapping)
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
