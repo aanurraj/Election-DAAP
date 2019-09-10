a7b1cdd228328a62209b237ef2ec68cbd73a8f2b
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2019-09-10 13:47:43 +0530

    Testing done using Mocha and Chai

--- test/election.js
+++ test/election.js
@@ -1,12 +1,28 @@
 var Election = artifacts.require("./Election.sol");
 
 contract("Election", function(accounts){
-
+    var electionInstance;
     it("Initilizes with two Candidates", function(){
         return Election.deployed().then(function(instance){
             return instance.candidatesCount();
         }).then(function(count){
             assert.equal(count, 2);
+        });
+    });
+
+    it("Initilizes with correct values", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            return electionInstance.candidates(1);
+        }).then(function(candidate){
+            assert.equal(candidate[0], 1, "Contains the correct id");
+            assert.equal(candidate[1], "Candidate 1", "Contains the correct candidate name");
+            assert.equal(candidate[2], 0, "Correct vote counts");
+            return electionInstance.candidates(2);
+        }).then(function(candidate){
+            assert.equal(candidate[0], 2, "Contains the correct id");
+            assert.equal(candidate[1], "Candidate 2", "Contains the correct candidate name");
+            assert.equal(candidate[2], 0, "Correct vote counts");
         });
     });
 });
